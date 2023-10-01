60c286212c7cdea50b674c2508ba00d615833dae
kallDun <igorxlonline24@gmail.com>
2023-10-01 16:16:29 +0300

    refactor some unit components

--- Source/TBS_Game/Public/Field/Unit/Components/MovementUnitComponent.h
+++ Source/TBS_Game/Public/Field/Unit/Components/MovementUnitComponent.h
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "MovementUnitComponent.generated.h"
 
+struct FHexagonLocation;
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class TBS_GAME_API UMovementUnitComponent : public UActorComponent
